a34402a08ef345abe03b94e4bba5583fb014e685
ashishxrox <ashishnair732@gmail.com>
2024-03-07 13:26:14 +0530

    Grammar mistake

--- frontend/src/components/navbar/NavBox.js
+++ frontend/src/components/navbar/NavBox.js
@@ -10,7 +10,7 @@ const NavBox = () => {
           <div className="card my-card" style={{width: "20rem", height:"15rem", boxShadow:"10px 10px 15px", display:"flex", justifyContent:"center", alignItems:"center"}}>
             <div className="card-body get-visa-box" style={{borderRadius:"15px", display:"flex", alignItems:"center", justifyContent:"space-between",flexDirection:"column", textAlign:"center"}}>
               {/* <div className="get-visa-body" style={{height:"100%", backgroundColor:"rgba(255,255,255,0.2)", backdropFilter:"blur(1.5px)",display:"flex", justifyContent:"space-around", flexDirection:"column"}}> */}
-                <h4 className="card-title" style={{fontSize:"40px", color:"#000047", textShadow:"2px 2px 5px #000" , padding:"5px 10px", fontFamily:"Trirong", fontStyle:"bold", backgroundColor:"rgba(255,255,255,0.2)", backdropFilter:"blur(1.5px)",}}>E-Visa in Just 3 Steps</h4>
+                <h4 className="card-title" style={{fontSize:"40px", color:"#000047", textShadow:"2px 2px 5px #000" , padding:"5px 10px", fontFamily:"Trirong", fontStyle:"bold", backgroundColor:"rgba(255,255,255,0.2)", backdropFilter:"blur(1.5px)",}}>E-Visa In Just 3 Steps</h4>
                 {/* <ion-icon name="newspaper-outline" style={{fontSize:"140px", color:"#2446B2"}}></ion-icon> */}
                 <Link to="/visa" className="btn btn-primary my-2 button" style={{fontSize:"24px", padding: "5px 40px",backgroundColor:"#000047", border:"1px solid #000047", boxShadow:"5px 5px 10px #000" ,width:"104%", transform:"translateX(-4px)"}}>Check It Out</Link>
               {/* </div> */}
--- frontend/src/components/visa/TandC.js
+++ frontend/src/components/visa/TandC.js
@@ -10,7 +10,7 @@ const TandC = ({ closeBtn }) => {
                 <ul style={{color:"#000"}}>
                     <li style={{listStyleType:"disc"}}>We are a Visa facilitator only and do not influence any visa delays or immigration issues if
                         occurred.</li>
-                    <li style={{listStyleType:"disc"}}>Visa issuance is the sole prerogative of a country's government. if Visa is refused or rejected due
+                    <li style={{listStyleType:"disc"}}>Visa issuance is the sole prerogative of a country's government. If Visa is refused or rejected due
                         to any reason, we will not be responsible for the same</li>
                     <li style={{listStyleType:"disc"}}>Please verify all Visa details, if any change or error on the Visa reported/sent to us after 2 hrs will
                         not be corrected/entertained.</li>
