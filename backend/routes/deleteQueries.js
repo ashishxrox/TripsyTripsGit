caedda57b74013d044be6b16e229a8446b0ac02f
ashishxrox <ashishnair732@gmail.com>
2024-03-15 20:16:58 +0530

    Adding Some queries

--- backend/routes/deleteQueries.js
+++ backend/routes/deleteQueries.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const FormData = require('../models/FormData')
+const Insurance = require('../models/Insurance')
+const Flight = require('../models/Flight')
+const NonEvisaFormData = require('../models/NonEvisaModel')
+const Package = require('../models/Package')
+const HotelData = require('../models/Hotel')
 
 const router = express.Router();
 
@@ -7,6 +12,57 @@ const router = express.Router();
 router.delete('/', async(req, res)=>{
     try {
         await FormData.deleteMany({})
+
+        res.status(200).json({message: "All entries deleted"})
+    } catch (error) {
+        res.status(500).json({error: "COuld not Delete!", details:error.message})
+    }
+})
+
+
+router.delete('/flight', async(req, res)=>{
+    try {
+        await Flight.deleteMany({})
+
+        res.status(200).json({message: "All entries deleted"})
+    } catch (error) {
+        res.status(500).json({error: "COuld not Delete!", details:error.message})
+    }
+})
+
+router.delete('/hotel', async(req, res)=>{
+    try {
+        await HotelData.deleteMany({})
+
+        res.status(200).json({message: "All entries deleted"})
+    } catch (error) {
+        res.status(500).json({error: "COuld not Delete!", details:error.message})
+    }
+})
+
+router.delete('/insurance', async(req, res)=>{
+    try {
+        await Insurance.deleteMany({})
+
+        res.status(200).json({message: "All entries deleted"})
+    } catch (error) {
+        res.status(500).json({error: "COuld not Delete!", details:error.message})
+    }
+})
+
+router.delete('/nonevisa', async(req, res)=>{
+    try {
+        await NonEvisaFormData.deleteMany({})
+
+        res.status(200).json({message: "All entries deleted"})
+    } catch (error) {
+        res.status(500).json({error: "COuld not Delete!", details:error.message})
+    }
+})
+
+router.delete('/package', async(req, res)=>{
+    try {
+        await Package.deleteMany({})
 
         res.status(200).json({message: "All entries deleted"})
     } catch (error) {
