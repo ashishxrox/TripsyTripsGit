2784141f6171b8caf945f9f01e217e325b5cff4a
ashishxrox <ashishnair732@gmail.com>
2024-03-18 19:53:22 +0530

    Fixing the footer

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -13,7 +13,9 @@ const Footer = () => {
     return (
         <div className='footer'>
             <div className="developer-crds" style={{ display: "flex", flexDirection: "column", justifyContent: "center", margin: "10px 0", alignItems: "center" }}>
-                <h6 style={{ padding: "0", fontSize: "18px", color:"#fff"}}>Developed by Ashish Nair</h6>
+            <h6 style={{ padding: "0", fontSize: "28px", color:"#fff"}}>Contact us</h6>
+            <p style={{ padding: "0", position: "relative", top: "-10px", fontSize: "8px", color:"#fff"}}>+91 88888 77729 | +91 88059 60110 | +91 88280 66947 | +91 81818 16662</p>
+                <h6 style={{ padding: "0", fontSize: "16px", color:"#fff"}}>Developed by Ashish Nair</h6>
                 <p style={{ padding: "0", position: "relative", top: "-10px", fontSize: "8px", color:"#fff"}}>+91 9823097992 | ashishnair732@gmail.com</p>
             </div>
             <div className="copyright-box" style={{ display: "flex", flexDirection: "column", justifyContent: "center", margin: "10px 0", alignItems: "center" }}>
