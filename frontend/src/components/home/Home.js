933016e927fab8a7235b018d4d467bae4e1c9b0d
ashishxrox <ashishnair732@gmail.com>
2024-07-30 17:25:47 +0530

    Temp Changes

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -58,7 +58,7 @@ const Home = ({ onLoadStart, onLoadComplete }) => {
 
       <HomeOurPackage />
       <FlightHotels />
-      <PlaceToVisit />
+      {/* <PlaceToVisit /> */}
       <Reviews />
       {/* <Contact /> */}
     </div>
